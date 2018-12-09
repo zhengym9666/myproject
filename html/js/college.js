3c14d0faf877537e0073da83fae0ff698f96f2e7
lyb <1040087877@qq.com>
2018-12-09 17:08:28 +0800

    修改项目名写死

--- html/js/college.js
+++ html/js/college.js
@@ -3,8 +3,11 @@
  */
 (function(a){	
 	$("#math").on('click',function(){
-		
-		var url="/gd_stu_dev/service/action/CollegeAction.action";
+        //获取路径
+        var pathName=window.document.location.pathname;
+		//截取，得到项目名称
+        var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
+		var url=projectName+"/service/action/CollegeAction.action";
 		alert(url); 
 		$.ajax({
 			type: "get",
