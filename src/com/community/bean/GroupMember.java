66db457613f370479add415dcdce007f99bde216
lyb <1040087877@qq.com>
2019-02-09 23:40:23 +0800

    修改登录bug

--- src/com/community/bean/GroupMember.java
+++ src/com/community/bean/GroupMember.java
@@ -9,69 +9,94 @@ import java.util.Date;
 * @desrciption		
 */
 public class GroupMember {
- private String stuNum;
- private String stuName;
- private String clubId;
- private String departmentId;
- private String rank;
- private String job;
- private String state;
- private Date joinTime;
- private Date exitTime;
-public String getStuNum() {
-	return stuNum;
-}
-public void setStuNum(String stuNum) {
-	this.stuNum = stuNum;
-}
-public String getStuName() {
-	return stuName;
-}
-public void setStuName(String stuName) {
-	this.stuName = stuName;
-}
-public String getClubId() {
-	return clubId;
-}
-public void setClubId(String clubId) {
-	this.clubId = clubId;
-}
-public String getDepartmentId() {
-	return departmentId;
-}
-public void setDepartmentId(String departmentId) {
-	this.departmentId = departmentId;
-}
-public String getRank() {
-	return rank;
-}
-public void setRank(String rank) {
-	this.rank = rank;
-}
-public String getJob() {
-	return job;
-}
-public void setJob(String job) {
-	this.job = job;
-}
-public String getState() {
-	return state;
-}
-public void setState(String state) {
-	this.state = state;
-}
-public Date getJoinTime() {
-	return joinTime;
-}
-public void setJoinTime(Date joinTime) {
-	this.joinTime = joinTime;
-}
-public Date getExitTime() {
-	return exitTime;
-}
-public void setExitTime(Date exitTime) {
-	this.exitTime = exitTime;
-}
- 
- 
+	private String stuNum;
+	private String stuName;
+	private String clubId;
+	private String departmentId;
+	private String rank;
+	private String job;
+	private String state;
+	private Date joinTime;
+	private Date exitTime;
+	private int id;
+
+	public String getStuNum() {
+		return stuNum;
+	}
+
+	public void setStuNum(String stuNum) {
+		this.stuNum = stuNum;
+	}
+
+	public String getStuName() {
+		return stuName;
+	}
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+
+	public String getClubId() {
+		return clubId;
+	}
+
+	public void setClubId(String clubId) {
+		this.clubId = clubId;
+	}
+
+	public String getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(String departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Date getJoinTime() {
+		return joinTime;
+	}
+
+	public void setJoinTime(Date joinTime) {
+		this.joinTime = joinTime;
+	}
+
+	public Date getExitTime() {
+		return exitTime;
+	}
+
+	public void setExitTime(Date exitTime) {
+		this.exitTime = exitTime;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
