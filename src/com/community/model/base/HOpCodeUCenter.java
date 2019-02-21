f7899e0055a6fcc5422fe7a65b7838db78e7a396
lyb <1040087877@qq.com>
2019-02-21 11:47:18 +0800

    聊天系统加表情功能的HOpCodeUCenter.java

---
+++ src/com/community/model/base/HOpCodeUCenter.java
@@ -0,0 +1,33 @@
+package com.community.model.base;
+
+
+public class HOpCodeUCenter {
+	public static String UC_ERROR = "0";
+
+	public static String CREATE_USER_GROUP = "1";
+	public static String UPDATE_USER_GROUP = "2";
+	public static String GET_USER_GROUP = "3";
+	public static String GET_USER_GROUP_LIST = "4";
+	public static String DELETE_USER_GROUP = "5";
+	public static String GET_USER_GROUP_TREE = "6";
+	public static String GET_USER_GROUP_RECURSION = "7";
+
+	public static String CREATE_USER = "10";
+	public static String GET_USER = "11";
+	public static String UPDATE_USER = "12";
+	public static String GET_USER_LIST = "13";
+	public static String GET_USER_IMG = "14";
+	public static String GET_USER_BY_EMAIL = "15";
+	public static String CHECK_USER_BY_USER_NAME = "16";
+	public static String CHECK_USER_PHONE = "17";
+
+	public static String GET_TOKEN = "20";
+	public static String UPDATE_TOKEN = "21";
+	public static String DELETE_TOKEN = "22";
+	public static String GET_ADMIN_TOKEN = "23";
+
+	public static void init() {
+
+
+	}
+}
